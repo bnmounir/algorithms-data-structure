3e15d55694d210e79cd2b8c1a892f5e507ce294c
Mounir Benabderrahmane <bnmounir7@gmail.com>
2020-03-12 17:03:25 -0400

    capitalize

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,45 +8,64 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+function cleanString(str) {
+    return str
+        .replace(/[^\w]/g, '')
+        .toLowerCase()
+        .split('')
+        .sort()
+        .join('');
+}
+
 function anagrams(stringA, stringB) {
-    // let strA = stringA.replace(/[^\w]/g, '').toLowerCase();
-    // let strB = stringB.replace(/[^\w]/g, '').toLowerCase();
-    // if (strA.length !== strB.length) return false;
-    // const objA = {};
-    // const objB = {};
-    // for (let char of strA) {
-    //     objA[char] ? objA[char]++ : (objA[char] = 1);
-    // }
-    // for (let char of strB) {
-    //     objB[char] ? objB[char]++ : (objB[char] = 1);
-    // }
-    // SOLUTION 1
-    // function buildCharMap(str) {
-    //     const charMap = {};
-    //     let cleanedStr = str.replace(/[^\w]/g, '').toLowerCase();
-    //     for (let char of cleanedStr) {
-    //         charMap[char] = charMap[char] + 1 || 1;
-    //     }
-    //     return charMap;
-    // }
-    // const objMapA = buildCharMap(stringA);
-    // const objMapB = buildCharMap(stringB);
-    // if (Object.keys(objMapA).length !== Object.keys(objMapB).length)
-    //     return false;
-    // for (let char in objMapA) {
-    //     if (objMapA[char] !== objMapB[char]) return false;
-    // }
-    // return true;
-    //SOLUTION 2
-    function cleanStr(str) {
-        return str
-            .replace(/[^\w]/g, '')
-            .toLowerCase()
-            .split('')
-            .sort()
-            .join();
-    }
-    return cleanStr(stringA) === cleanStr(stringB);
+    const strA = cleanString(stringA);
+    const strB = cleanString(stringB);
+    return strA === strB;
 }
 
 module.exports = anagrams;
+
+// SOLUTION 1
+// function cleanString(str) {
+//     return str.replace(/[^\w]/g, '').toLowerCase();
+// }
+// function mapBuilder(str) {
+//     const newObj = {};
+//     for (let char of str) {
+//         !newObj[char] ? (newObj[char] = 1) : newObj[char]++;
+//     }
+//     return newObj;
+// }
+
+// function anagrams(stringA, stringB) {
+//     const strA = cleanString(stringA);
+//     const strB = cleanString(stringB);
+//     if (strA.length !== strB.length) return false;
+//     const mapA = mapBuilder(strA);
+//     const mapB = mapBuilder(strB);
+//     console.log(mapA);
+//     console.log(mapB);
+//     for (let char in mapA) {
+//         if (mapA[char] !== mapB[char]) return false;
+//     }
+//     return true;
+// }
+//SOLUTION 2
+// function cleanStr(str) {
+//     return str
+//         .replace(/[^\w]/g, '')
+//         .toLowerCase()
+//         .split('')
+//         .sort()
+//         .join();
+// }
+// return cleanStr(stringA) === cleanStr(stringB);
+
+//SOLUTION 3
+// const strA = cleanString(stringA);
+//     const strB = cleanString(stringB);
+//     if (strA.length !== strB.length) return false;
+//     for (let char of strA) {
+//         if (!strB.includes(char)) return false;
+//     }
+//     return true;
--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,17 +8,40 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-    // SOLUTION 1
-    // return str
-    //     .split(' ')
-    //     .map(chunk => chunk.charAt(0).toUpperCase() + chunk.slice(1))
-    //     .join(' ');
-    // SOLUTION 2
-    let res = str[0].toUpperCase();
+    let newStr = str[0].toUpperCase();
     for (let i = 1; i < str.length; i++) {
-        str[i - 1] === ' ' ? (res += str[i].toUpperCase()) : (res += str[i]);
+        str[i - 1] === ' '
+            ? (newStr += str[i].toUpperCase())
+            : (newStr += str[i]);
     }
-    return res;
+    return newStr;
 }
+capitalize('i see hello my friend');
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+// SOLUTION 1
+// return str
+//     .split(' ')
+//     .map(chunk => chunk.charAt(0).toUpperCase() + chunk.slice(1))
+//     .join(' ');
+// SOLUTION 2
+//     let res = str[0].toUpperCase();
+//     for (let i = 1; i < str.length; i++) {
+//         str[i - 1] === ' ' ? (res += str[i].toUpperCase()) : (res += str[i]);
+//     }
+//     return res;
+// }
+// SOL3
+// return str
+//     .split(' ')
+//     .map(e => e.charAt(0).toUpperCase() + e.slice(1))
+//     .join(' ');
+//SOL 4
+// let newStr = str.split('').reduce((acc, cur, idx, srcArr) => {
+//     if (idx === 0 || srcArr[idx - 1] === ' ')
+//         return (acc += cur.toUpperCase());
+//     return (acc += cur);
+// }, '');
+// return newStr;
