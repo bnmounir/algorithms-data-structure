0812079012bc6cdeb45cd30413b39f9aad855d8e
Mounir Benabderrahmane <bnmounir7@gmail.com>
2020-03-16 21:52:19 -0400

    fib recursion

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,12 +8,46 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-    const resArr = [0,1]
-    for (let i=2; i<=n; i++) {
-        resArr.push(resArr[i-2]+resArr[i-1])
-    }
-    return resArr[n]
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        if (cache[args]) return cache[args];
+        const result = fn.call(this, ...args);
+        cache[args] = result;
+        return result;
+    };
 }
 
+function slowFib(n) {
+    if (n < 2) return n;
+    return fib(n - 2) + fib(n - 1);
+}
+
+const fib = memoize(slowFib);
+// console.time('n-1 + n-2');
+// fib(39);
+// console.timeEnd('n-1 + n-2');
+
 module.exports = fib;
+
+// function fib(n) {
+//     const resArr = [0,1]
+//     for (let i=2; i<=n; i++) {
+//         resArr.push(resArr[i-2]+resArr[i-1])
+//     }
+//     return resArr[n]
+// }
+
+// Solution 2 recursion
+// function fib(n, i = 2, resArr = [0, 1]) {
+//     let newValue = resArr[i - 2] + resArr[i - 1];
+//     resArr.push(newValue);
+//     if (n <= i) {
+//         return resArr[n];
+//     }
+//     //otherwise call the function again
+//     return fib(n, i + 1, resArr);
+// }
+// console.time('arr method');
+// fib(4000);
+// console.timeEnd('arr method');
